ffcf3b0cfd61c84cb0aaddc30425fa2059cd493c
Nguyễn Hữu Đồng <huudong03uet@gmail.com>
2023-04-02 08:58:50 +0700

    create project

--- admin_fashion_book-master/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import bookReducers from './book.reducer'
-import userReducers from './user.reducer'
-import homeReducers from './home.reducer';
-export default combineReducers({
-    bookReducers,
-    userReducers,
-    homeReducers
-})
--- fashion_book_backend-master/api/routers/comment.router.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const comment_controller = require('../controllers/comment.controller');
-module.exports = (app) => {
-    app.route('/comment')
-        .post(comment_controller.mycomment);
-    app.route('/comment/book')
-        .post(comment_controller.getCommentByIDBook)
-}
