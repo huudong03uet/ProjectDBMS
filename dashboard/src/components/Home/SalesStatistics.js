8cb3263092d38bcec7a134a85abf3671707e2713
Uyen0408 <21020419@vnu.edu.vn>
2023-04-08 00:45:28 +0700

    fix chart and dashboard side bar

--- dashboard/src/components/Home/SalesStatistics.js
+++ dashboard/src/components/Home/SalesStatistics.js
@@ -7,15 +7,16 @@ const SaleStatistics = () => {
         <article className="card-body">
           <h5 className="card-title">Sale statistics</h5>
           <iframe 
-            style={{background: "#FFFFFF",
+            style={{
+              background: "#FFFFFF",
               border: "none",
               borderRadius: "2px",
-              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
-              width:"100%",
-              height:"350px",
+              boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2);",
+              width: "100%",
+              height: "350px",
             }}
-              src="https://charts.mongodb.com/charts-ecommerce-eilib/embed/charts?id=642b12f5-d0f4-41e9-896d-5ed4eba3a606&maxDataAge=3600&theme=light&autoRefresh=true"
-              ></iframe>
+            src="https://charts.mongodb.com/charts-ecommerce-eilib/embed/charts?id=642b12f5-d0f4-41e9-896d-5ed4eba3a606&maxDataAge=3600&theme=light&autoRefresh=true"
+            ></iframe>
         </article>
       </div>
     </div>
